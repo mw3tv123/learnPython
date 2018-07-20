4557241eafeb182e1e0f5e37255b3e64d74701ff
Hung Tran Quoc <tqhung1@tma.com.vn>
2018-07-20 17:48:49 +0700

    update repo

--- CompletedPY/Section2-Exercises/ex01.py
+++ CompletedPY/Section2-Exercises/ex01.py
@@ -1,9 +1,8 @@
-"""
-Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
-Suppose the following input is supplied to the program:
-34,67,55,33,12,98
-Then, the output should be:
-['34', '67', '55', '33', '12', '98']
-('34', '67', '55', '33', '12', '98')
-ET 30m
-"""
+"""Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple
+which contains every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98 Then,
+the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98') ET 30m """
+
+data = input(">>>Enter your number sequence: ")
+_list = list(data.split(','))
+_tuple = tuple(data.split(','))
+print(_list, _tuple)
--- CompletedPY/Section2-Exercises/ex02.py
+++ CompletedPY/Section2-Exercises/ex02.py
@@ -1,7 +1,29 @@
-'''
+"""
 Define a class which has at least two methods:
 getString: to get a string from console input
 printString: to print the string in upper case.
 Also please include simple test function to test the class methods.
 ET 30m
-'''
+"""
+
+
+class MyString:
+    """Simple string method"""
+
+    def __init__(self, string):
+        self.string = string
+
+    def getString(self):
+        self.string = input(">>> Enter a string: ")
+
+    def printString(self):
+        return self.string.capitalize()
+
+    def testCase(self):
+
+
+
+if __name__ == "__main__":
+    s = MyString()
+    s.getString()
+    print(s.printString())
