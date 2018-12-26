c75824f2657fb5fb12ce44c52c7689eff5d485b0
Hung <mw3tv123@gmail.com>
2018-12-26 14:02:12 +0700

    Remove some file and create youtube-downloader project

--- flask-project/app/routes.py
+++ flask-project/app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
-@app.route('index')
+@app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Hung'}
+    posts = [
+        {
+            'author': {'username': 'Nick'},
+            'body': 'Beautiful day in England!'
+        },
+        {
+            'author': {'username': 'Niko'},
+            'body': 'Nice day for a walk!'
+        },
+        {
+            'author': {'username': 'Reg'},
+            'body': 'Keep moving into the Storm!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
